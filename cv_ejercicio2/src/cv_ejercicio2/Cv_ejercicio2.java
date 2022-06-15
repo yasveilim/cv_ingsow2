17322ccb89db1c200e1564db5be4f6e512989aa4
MONICA MENDOZA <MONICA MENDOZA@LAPTOP-B4MNL0GH>
2022-06-14 20:13:20 -0500

    Versión 1.1

--- cv_ejercicio2/src/cv_ejercicio2/Cv_ejercicio2.java
+++ cv_ejercicio2/src/cv_ejercicio2/Cv_ejercicio2.java
@@ -17,7 +17,7 @@ public class Cv_ejercicio2 {
     public static void main(String[] args) {
         // TODO code application logic here
         //Versión 1
-        //Versión 1.1
+        //Versión 1.1.
     }
     
 }
